972e8ef02686cba868219f9a580d0f71dfcc65bd
Andrew Male <amale@zibings.com>
2014-06-30 21:03:50 -0400

    Adding links to socket tutorials for overlapped I/O on both Linux and Windows

--- source/src/fw.cpp
+++ source/src/fw.cpp
@@ -2,6 +2,10 @@
 
 #include <fw.h>
 
+// TODO: Implement Socket stuff...
+//       Windows: http://www.winsocketdotnetworkprogramming.com/winsock2programming/winsock2advancediomethod5e.html
+//       Linux: http://www.tidytutorials.com/2012/06/linux-c-overlapped-server-and-client.html
+
 int main()
 {
 	return 0;
