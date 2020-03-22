9e4c3ff4467a06f8e4c176ab6b13e94b438dbaa6
Andrew Male <mamale@outlook.com>
2020-03-22 16:17:55 -0400

    Attempting to add a build file with Discord notifications

---
+++ build/Get-SystemInfo.ps1
@@ -0,0 +1,31 @@
+<#
+
+.SYNOPSIS
+Retrieve a set of information about the current system.
+
+.DESCRIPTION
+Retrieves and displays several pieces of information about the current system.
+
+.EXAMPLE
+Show information about the current system.
+
+bin/Get-SystemInfo.ps1
+
+#>
+Write-Host "-> Gathering system information"
+Write-Host ""
+Write-Host "Computer Name: $($env:computername)"
+Get-WmiObject -Class Win32_LogicalDisk
+
+$uptime = (get-date) - (gcim Win32_OperatingSystem).LastBootUpTime
+Write-Host "Host uptime: $($uptime)"
+
+$os = Get-Ciminstance Win32_OperatingSystem
+$os | Select-Object @{Name = "MEM FreeGB";Expression = {[math]::Round($_.FreePhysicalMemory/1mb,2)}},
+	@{Name = "MEM TotalGB";Expression = {[int]($_.TotalVisibleMemorySize/1mb)}}
+
+Invoke-Expression "php -v"
+Invoke-Expression "composer --version"
+Write-Host "PS Version: $($PSVersionTable.PSVersion)"
+Write-Host ""
+Write-Host "-> Done gathering system information"
