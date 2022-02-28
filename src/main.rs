ad01132f9ad6dc04fbff46dbdef6fcb3ff4da2a3
Andrew Male <mamale@outlook.com>
2022-02-27 22:20:51 -0500

    Formatting fix in main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod fw;
 
-use fw::model::character::{Character, build_empty_character, build_character_from_vnum};
+use fw::model::character::{ Character, build_empty_character, build_character_from_vnum };
 
 fn main() {
     println!("Hello, world!");
+
     let mut a_char = build_empty_character();
     a_char.Vnum = 202;
 
