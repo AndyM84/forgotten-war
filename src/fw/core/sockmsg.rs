c37e496701975cebe85578f69b6c1fed3ec196d5
Andrew Male <mamale@outlook.com>
2022-03-31 03:48:01 -0400

    Semi-functional TCP server w/ note

---
+++ src/fw/core/sockmsg.rs
@@ -0,0 +1,4 @@
+pub struct SockMsg {
+	pub fd: usize,
+	pub msg: String
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 mod fw;
 
+use fw::core::sockmsg::SockMsg;
 use fw::model::character::{ Character };
 
+use std::collections::HashMap;
 use std::net::{TcpListener, TcpStream, Shutdown, SocketAddr};
 use std::io::{ErrorKind, Read, Write};
 use std::thread::{JoinHandle, sleep};
 use std::time::Duration;
-use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc::{Sender, Receiver, TryRecvError};
 use std::sync::mpsc;
 use std::thread;
 
-struct SockMsg {
-    fd: SocketAddr,
-    msg: String
-}
-
-fn handle_client(mut stream: TcpStream, chan: Sender<SockMsg>) {
+fn handle_client(mut stream: TcpStream, fd: usize, chan: Sender<SockMsg>) {
     let mut data = [0 as u8; 50]; // using 50 byte buffer
     let mut last_size = 0 as usize;
 
@@ -35,21 +32,30 @@ fn handle_client(mut stream: TcpStream, chan: Sender<SockMsg>) {
     } {
         if last_size > 0 {
             chan.send(SockMsg {
-                fd: stream.peer_addr().unwrap(),
-                msg: String::from_utf8(Vec::from(&data[0..last_size])).unwrap()
+                fd,
+                msg: String::from_utf8(Vec::from(&data[0..last_size])).unwrap().replace("\r\n", "").replace("\n", "")
+            }).unwrap();
+        } else if last_size == 0 {
+            stream.shutdown(Shutdown::Both).unwrap();
+
+            chan.send(SockMsg {
+                fd,
+                msg: String::new()
             }).unwrap();
         }
     }
 }
 
 fn main() {
-    // NOTE!!
-    //   - Might try making a thread/channel just for accepting sockets and process the accepted
-    //     sockets later on in the main loop?
+    // NOTE:
+    //  - Need to look into Arc<T> to see if we can use pointers to share a message queue instead
+    //    of these damn channels.
 
+    let (listen_send, listen_recv): (Sender<TcpStream>, Receiver<TcpStream>) = mpsc::channel();
     let (chan_sender, chan_recvr): (Sender<SockMsg>, Receiver<SockMsg>) = mpsc::channel();
     let mut chars: Vec<Character>        = Vec::new();
     let mut threads: Vec<JoinHandle<()>> = Vec::new();
+    let mut conns: HashMap<usize, usize> = HashMap::new();
 
     let listener = TcpListener::bind("0.0.0.0:6055").unwrap();
     println!("Server listening on port 6055");
@@ -58,28 +64,35 @@ fn main() {
         for stream in listener.incoming() {
             match stream {
                 Ok(stream) => {
-                    println!("New connection: {}", stream.peer_addr().unwrap());
-
-                    let chan_send = chan_sender.clone();
-
-                    threads.push(thread::spawn(move || {
-                        handle_client(stream, chan_send);
-                    }));
+                    listen_send.send(stream).unwrap();
                 }
                 Err(e) if e.kind() == ErrorKind::WouldBlock => {
                     continue;
                 }
                 Err(e) => {
-                    //println!("Error: {}", e);
-                    /* connection failed */
+                    println!("Error: {}", e);
                 }
             }
         }
     });
 
     loop {
-        for _ in 0..chars.len() {
-            if let msg = chan_recvr.recv_timeout(Duration::new(0, 5000000)).unwrap() {
+        for stream in listen_recv.try_recv() {
+            println!("New connection from {}", stream.peer_addr().unwrap());
+
+            let new_fd = chars.len() + 1;
+            chars.push(Character::new_from_vnum(new_fd.clone() as u32));
+            let chan_send = chan_sender.clone();
+
+            threads.push(thread::spawn(move || {
+                handle_client(stream, new_fd, chan_send);
+            }));
+        }
+
+        for msg in chan_recvr.try_iter() {
+            if msg.msg.len() < 1 {
+                println!("Client #{} disconnected, cleaning them up", msg.fd);
+            } else {
                 println!("{}:{}", msg.fd, msg.msg);
             }
         }
